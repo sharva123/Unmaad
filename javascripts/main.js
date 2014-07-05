8ac368982d2ca6401f5c0e3ac6d59dcbedcf523e
sharva123 <sharva@guerrillamail.com>
2014-07-05 17:14:42 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
 console.log('This would be the main JS file.');
+var sliderOptions =
+{
+	sliderId: "slider",
+	startSlide: 0,
+	effect: "series1",
+	effectRandom: false,
+	pauseTime: 2800,
+	transitionTime: 1200,
+	slices: 14,
+	boxes: 8,
+	hoverPause: 1,
+	autoAdvance: true,
+	captionOpacity: 0.4,
+	captionEffect: "fade",
+	thumbnailsWrapperId: "thumbs",
+	m: false,
+	license: "mylicense"
+};
