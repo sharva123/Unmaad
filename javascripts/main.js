ed07958c0a6e6b9b323d25b12776464284773a85
sharva123 <sharva@guerrillamail.com>
2014-07-06 13:01:35 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,30 +21,35 @@ var sliderOptions =
 
 
 
-(function(){
+function(){
+  
+var counter = 0, // to keep track of current slide
+    $items = document.querySelectorAll('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
+    numItems = $items.length; // total number of slides
 
-var counter = 0,
-$items = document.querySelectorAll('.diy-slideshow figure'),
-numItems = $items.length;
-
+// this function is what cycles the slides, showing the next or previous slide and hiding all the others
 var showCurrent = function(){
-var itemToShow = Math.abs(counter%numItems);
-
-[].forEach.call( $items, function(el){
-el.classList.remove('show');
-});
-
-$items[itemToShow].classList.add('show');
+  var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
+  
+  // remove .show from whichever element currently has it 
+  // http://stackoverflow.com/a/16053538/2006057
+  [].forEach.call( $items, function(el){
+    el.classList.remove('show');
+  });
+  
+  // add .show to the one item that's supposed to have it
+  $items[itemToShow].classList.add('show');    
 };
 
+// add click events to prev & next buttons 
 document.querySelector('.next').addEventListener('click', function() {
-counter++;
-showCurrent();
-}, false);
+     counter++;
+     showCurrent();
+  }, false);
 
 document.querySelector('.prev').addEventListener('click', function() {
-counter--;
-showCurrent();
-}, false);
-
-})();
+     counter--;
+     showCurrent();
+  }, false);
+  
+})();  
